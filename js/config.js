c02ac9c7e284b2457dee8c1c044af34ed84227e1
yasgur99 <michaelmaitland@verizon.net>
2019-03-25 01:11:24 -0400

    mailchimp url

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 "use strict"
-var subscribeUrl = "http://octrace.us14.list-manage.com/subscribe/post?u=b65c95763e0ae8e0c14e7af0e&amp;id=7e9cf89638";
+var subscribeUrl = "http://eepurl.com/glJGBP";
 var email = "michael@mer.gg";
 var countdown = false;
 var configDate = "2019 03 25";
