a690be45bd8e8c54d207fd19e6fca1c9590b9f09
yasgur99 <michaelmaitland@verizon.net>
2019-03-25 01:12:27 -0400

    mailchimp https

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 "use strict"
-var subscribeUrl = "http://eepurl.com/glJGBP";
+var subscribeUrl = "https://eepurl.com/glJGBP";
 var email = "michael@mer.gg";
 var countdown = false;
 var configDate = "2019 03 25";
